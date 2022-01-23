0992e0d0d46a117d54342245e5c342c0a7dabd3e
PhoenixWyllow <54237626+PhoenixWyllow@users.noreply.github.com>
2022-01-23 16:43:02 +0100

    minor fixes

--- MacroDeck.SoundPad/MacroDeck.SoundPad/Models/Localization.cs
+++ MacroDeck.SoundPad/MacroDeck.SoundPad/Models/Localization.cs
@@ -4,12 +4,11 @@
     {
         public string Attribution { get; set; } = "built-in values";
         public string Language { get; set; } = "English (default)";
-        public string PluginName { get; set; } = "SoundPad plugin";
         public string PluginDescription { get; set; } = "A SoundPad integration for Macro Deck";
         public string Connected { get; set; } = "SoundPad Connected";
         public string Disconnected { get; set; } = "SoundPad Disconnected";
         public string PlayActionName { get; set; } = "Play";
         public string PlayActionDescription { get; set; } = "Play a sound";
-        public string PlayActionAudioIndex { get; internal set; } = "Audio index";
+        public string PlayActionAudioIndex { get; set; } = "Audio index";
     }
 }

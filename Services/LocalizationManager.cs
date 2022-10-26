24db089aeba038986c953b3a3d659cfa56cd606b
Will <54237626+PhoenixWyllow@users.noreply.github.com>
2022-10-26 17:40:59 +0200

    fix language not changed

--- Services/LocalizationManager.cs
+++ Services/LocalizationManager.cs
@@ -61,8 +61,8 @@ internal static class LocalizationManager
         {
             languageName = "English"; //This should always be present as default, otherwise the code goes to fallback implementation.
         }
-
-        string languageFileName = $"PW.MacroDeck.SoundPad.Languages.{languageName}.json";
+        
+        string languageFileName = $"PW.MacroDeck.Soundpad.Languages.{languageName}.json";
 
         using var resourceStream = assembly.GetManifestResourceStream(languageFileName);
         using var streamReader = new StreamReader(resourceStream);
